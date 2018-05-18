8caf2469a0b061d7d6ace69ccad545b8422452a8
Hunter Perrin <hperrin@gmail.com>
2018-05-18 11:33:55 -0700

    Version bump.

--- src/Nymph.php
+++ src/Nymph.php
@@ -14,7 +14,7 @@ use Nymph\Drivers\DriverInterface;
  * @link http://nymph.io/
  */
 class Nymph {
-  const VERSION = '3.0.0-beta.13';
+  const VERSION = '3.0.0-beta.14';
 
   /**
    * The Nymph config array.
